acfeca54ab8b26b95f1abf292ca8b7b56c793c95
ankanmaiti <ankanmaiti2@gmail.com>
2024-04-24 09:49:40 +0530

    update: asyncHandler modified

--- src/helpers/asyncHandler.ts
+++ src/helpers/asyncHandler.ts
@@ -1,7 +1,24 @@
+import { NextResponse } from "next/server";
 import { ApiError } from "./apiError";
 import { ApiResponse } from "./apiResponse";
 
-function JsonResponse(res: ApiResponse | ApiError) {
+function JsonResponse(res: any) {
+  // return customize response
+  const resIsApiResponse = res instanceof ApiResponse;
+  const resIsApiError = res instanceof ApiError;
+  const resIsNextResponse = res instanceof NextResponse;
+
+  if (resIsNextResponse) {
+    return res;
+  }
+
+  if (!resIsApiResponse && !resIsApiError) {
+    console.log("unknown response type : ", typeof res);
+    console.log("response : ", res);
+    throw new Error("unknown response type");
+  }
+
+  // default response
   return Response.json(
     {
       statusCode: res.statusCode,
@@ -11,21 +28,18 @@ function JsonResponse(res: ApiResponse | ApiError) {
     },
     {
       status: res.statusCode,
+      headers: {
+        "Content-Type": "application/json",
+      },
     },
   );
 }
 
-export function asyncHandler(asyncCallback: (...args: any[]) => Promise<ApiResponse>) {
-  return async (...args: any[]) => {
-    try {
-      const res: ApiResponse = await asyncCallback(...args);
-      return JsonResponse(res);
-    } catch (err) {
-      // got ApiError then send response
-      if (err instanceof ApiError) {
-        return JsonResponse(err);
-      }
-      console.error("error: ", err);
-    }
-  };
+type AsyncCallback = (...args: any[]) => Promise<any>;
+
+export function asyncHandler(asyncCallback: AsyncCallback) {
+  return async (...args: any[]) =>
+    Promise.resolve(asyncCallback(...args))
+      .then(JsonResponse)
+      .catch(JsonResponse);
 }
